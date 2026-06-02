c149f04e6e89198cbac783d770386f18fff98eb2
mudit_1711 <muditagrawal06@gmail.com>
2026-06-02 13:11:45 +0530

    added some animations and better UI

--- app.py
+++ app.py
@@ -4,21 +4,23 @@ import numpy as np
 
 model = pickle.load(open("model.pkl", "rb"))
 
-st.title("Titanic Survival Prediction")
+st.title("🚢 Titanic Survival Prediction 🚢")
 
-pclass = st.number_input("Passenger Class", 1, 3)
+pclass = st.number_input("Passenger Class 🎫", 1, 3)
 age = st.number_input("Age", 1, 100)
-fare = st.number_input("Fare", 0) 
-sex = st.selectbox("Gender", ["male", "female"])
-sex = 1 if sex == "male" else 0
+fare = st.number_input("Fare(in £) 💰", 0) 
+sex = st.selectbox("Gender 🚻", ["male ♂️", "female ♀️"])
+sex = 1 if sex == "male ♂️" else 0
 
-if st.button("Predict"):
+if st.button("Predict 🚀"):
 
-    data = np.array([[pclass, age, fare,sex]])
+    data = np.array([[pclass, age, fare,sex]]) 
 
     prediction = model.predict(data)
 
     if prediction[0] == 1:
-        st.success("Survived")
+        st.success("Survived 🥳🎉")
+        st.balloons() 
     else:
-        st.error("Not Survived")
+        st.error("Not Survived ⚰️💀")
+        st.snow() 
