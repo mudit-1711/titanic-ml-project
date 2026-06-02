edf59ba24e9ef972f7d3f9400205ab68a358f25f
mudit_1711 <muditagrawal06@gmail.com>
2026-06-02 15:33:56 +0530

    made more accurate

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 df = pd.read_csv("data/train.csv")
@@ -21,7 +21,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
-model = LogisticRegression()
+model = RandomForestClassifier( n_estimators=100,
+    random_state=42
+)
 
 model.fit(X_train, y_train)
 
