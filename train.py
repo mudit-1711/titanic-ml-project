ef166c14203694f094dfbeb3d34665236255a6c3
mudit_1711 <muditagrawal06@gmail.com>
2026-07-02 19:57:05 +0530

    improved

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 import pickle
 
 df = pd.read_csv("data/train.csv")
@@ -17,11 +18,13 @@ X["Fare"] = X["Fare"].fillna(X["Fare"].mean())
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
-    test_size=0.2,
+    test_size=0.4,
     random_state=42
 )
 
-model = RandomForestClassifier( n_estimators=100,
+model = XGBClassifier( n_estimators=100,
+    learning_rate=0.1,
+    max_depth=5,
     random_state=42
 )
 
